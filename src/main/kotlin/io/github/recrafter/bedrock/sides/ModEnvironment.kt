3ea3faf0fedf61484038508900130668f47302bc
diskria <diskria@proton.me>
2025-11-15 08:25:07 +0400

    feat: release 0.1.3
    - add environment for server only mods

--- src/main/kotlin/io/github/recrafter/bedrock/sides/ModEnvironment.kt
+++ src/main/kotlin/io/github/recrafter/bedrock/sides/ModEnvironment.kt
@@ -1,14 +1,8 @@
 package io.github.recrafter.bedrock.sides
 
 enum class ModEnvironment(val sides: List<ModSide>) {
-
-    CLIENT_SERVER(
-        listOf(ModSide.CLIENT, ModSide.SERVER)
-    ),
-    CLIENT(
-        listOf(ModSide.CLIENT)
-    ),
-    DEDICATED_SERVER(
-        listOf(ModSide.SERVER)
-    ),
+    CLIENT_SERVER(listOf(ModSide.CLIENT, ModSide.SERVER)),
+    CLIENT_ONLY(listOf(ModSide.CLIENT)),
+    SERVER_ONLY(listOf(ModSide.SERVER)),
+    DEDICATED_SERVER_ONLY(listOf(ModSide.SERVER)),
 }
--- src/main/kotlin/io/github/recrafter/bedrock/sides/ModSide.kt
+++ src/main/kotlin/io/github/recrafter/bedrock/sides/ModSide.kt
@@ -1,13 +1,6 @@
 package io.github.recrafter.bedrock.sides
 
-import io.github.diskria.kotlin.utils.extensions.mappers.getName
-import io.github.diskria.kotlin.utils.words.PascalCase
-
 enum class ModSide {
-
     CLIENT,
     SERVER;
-
-    fun getRunTaskName(): String =
-        "run" + getName(PascalCase)
 }
